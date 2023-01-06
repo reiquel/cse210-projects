97c5ae1be071d1467ff2751f438ed5ec4a4f41c7
reiquel <carelary92@gmail.com>
2023-01-05 22:54:55 -0300

    We added the two new proyects requested in the first week of class.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade? ");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+        string letter = " ";
+        if (grade >= 90){
+            letter = "A";
+        }
+        else if (grade >= 80){
+            letter = "B";
+        }
+        else if (grade >= 70){
+            letter = "C";
+        }
+        else if (grade >= 60){
+            letter = "D";
+        }
+        else  {
+            letter = "F";
+        }
+        Console.WriteLine($"Your grade is {letter}");
+        if (grade >= 70){
+            Console.WriteLine("You passed the class, congratulations!");
+        }
+        else {
+            Console.WriteLine("We encorage you to keep on trying, you did not pass the class this time");
+        }
     }
 }
